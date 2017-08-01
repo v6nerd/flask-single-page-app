42d2ddfecc9641d0819e831aa617522e2693ee1f
v6nerd <mhabib1981@gmail.com>
2017-08-01 17:11:35 +0200

    basic

--- app.py
+++ app.py
@@ -6,24 +6,24 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-requests_cache.install_cache('github_cache', backend='sqlite', expire_after=180)
+requests_cache.install_cache('crowdstrike_cache', backend='sqlite', expire_after=42360)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    if request.method == 'POST':
+    if request.method == 'GET':
         # user inputs
-        first = request.form.get('first')
-        second = request.form.get('second')
+        ip = request.args.get('ip')
+        #second = request.form.get('second')
         # api call
-        url = "https://api.github.com/search/users?q=location:{0}+language:{1}".format(first, second)
+        url = "https://www.threatcrowd.org/searchApi/v2/ip/report/?ip={0}".format(ip)
         now = time.ctime(int(time.time()))
         response = requests.get(url)
         print("Time: {0} / Used Cache: {1}".format(now, response.from_cache))
         # return json
-        return jsonify(response.json())
-    return render_template('index.html')
+    return jsonify(response.json())
+    #return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run('0.0.0.0',debug=True)
